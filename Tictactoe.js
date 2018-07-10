6534beb871c5103f73afac4b4ef58c5d78e5e0c2
William Leung <leungwk25@gmail.com>
2018-07-09 20:21:16 -0700

    Add Tictactoe Class as separate file

---
+++ Tictactoe.js
@@ -0,0 +1,19 @@
+class Tictactoe extends Board{
+	constructor(){
+		super();
+		this.playerOneTurn = true;
+	}
+
+	isPlayerOneTurn(){
+		return this.playerOneTurn;
+	}
+
+	setPlayerTurn(){
+		this.playerOneTurn = !this.playerOneTurn;
+	}
+
+	hasTie(){
+		return this.grid.filter( square => typeof square !== 'number').length === 0;
+	}
+
+}
