656dce1135b01a17c360f7c8e30401e1f8e926d4
William Leung <leungwk25@gmail.com>
2018-07-09 23:05:33 -0700

    Revise testing file

--- test/testClass.js
+++ test/testClass.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
 
-const {Human} = require('../Human.js');
-const {Computer} = require('../Computer.js');
-const {Tictactoe} = require('../Tictactoe.js');
+const Human = require('../Human.js');
+const Computer = require('../Computer.js');
+const Tictactoe = require('../Tictactoe.js');
 
 
 describe("Tic Tac Toe App Test",function() {
@@ -12,21 +12,22 @@ describe("Tic Tac Toe App Test",function() {
   let computer = new Computer('Computer', 'o');
 
   before(function() {
-     testBoard = Gameboard();
-     testBoard.resetGame();
-  })
+     testBoard = new Tictactoe();
+  });
 
 	describe("Starting a game",function() {
 		it("should have total moves of 9", function() {
       let len =testBoard.getBoard().length
 	     assert.deepEqual(len, 9);
 	  });
+
     it("should contain array with integers 0-8", function() {
     assert.deepEqual(testBoard.getBoard(), [0,1,2,3,4,5,6,7,8]);
     });
+
     it("should have first move by Player 1", function() {
-    assert.deepEqual(testBoard.isPlayer1Turn(), true);
+    assert.deepEqual(testBoard.isPlayerOneTurn(), true);
     });
 	});
 
-}
+});
