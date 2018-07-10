a89ca05d12a6c19711b4783583c7b37c247592fb
William Leung <leungwk25@gmail.com>
2018-07-09 23:15:23 -0700

    Add testing for Player1 first move

--- test/testClass.js
+++ test/testClass.js
@@ -4,30 +4,49 @@ const Human = require('../Human.js');
 const Computer = require('../Computer.js');
 const Tictactoe = require('../Tictactoe.js');
 
-
-describe("Tic Tac Toe App Test",function() {
+describe("Tic Tac Toe App Test", function() {
 	let testBoard;
-  let playerOne = new Human('Player1', 'x');
-  let playerTwo = new Human('Player2', 'o');
-  let computer = new Computer('Computer', 'o');
+	let playerOne = new Human('Player1', 'x');
+	let playerTwo = new Human('Player2', 'o');
+	let computer = new Computer('Computer', 'o');
 
-  before(function() {
-     testBoard = new Tictactoe();
-  });
+	before(function() {
+		testBoard = new Tictactoe();
+	});
 
-	describe("Starting a game",function() {
+	describe("Starting a game", function() {
 		it("should have total moves of 9", function() {
-      let len =testBoard.getBoard().length
-	     assert.deepEqual(len, 9);
-	  });
+			let len = testBoard.getBoard().length
+			assert.deepEqual(len, 9);
+		});
 
-    it("should contain array with integers 0-8", function() {
-    assert.deepEqual(testBoard.getBoard(), [0,1,2,3,4,5,6,7,8]);
-    });
+		it("should contain array with integers 0-8", function() {
+			assert.deepEqual(testBoard.getBoard(), [0, 1, 2, 3, 4, 5, 6, 7, 8]);
+		});
 
-    it("should have first move by Player 1", function() {
-    assert.deepEqual(testBoard.isPlayerOneTurn(), true);
-    });
+		it("should have first move by Player 1", function() {
+			assert.deepEqual(testBoard.isPlayerOneTurn(), true);
+		});
+	});
+
+
+	describe("Player1's first move",function() {
+		it("should have a cell fill by Player1's move", function() {
+			playerOne.setSquare(testBoard.getBoard(), 2);
+			assert.deepEqual(testBoard.getBoard()[2],playerOne.marker);
+		});
+		it("should be a data type string by Player1's move", function() {
+			assert.deepEqual(typeof testBoard.getBoard()[0],'number');
+			assert.deepEqual(typeof testBoard.getBoard()[1],'number');
+			assert.deepEqual(typeof testBoard.getBoard()[2],'string');
+		});
+		it("should not end the game", function() {
+			assert.deepEqual(testBoard.isGameOver(playerOne.isWinner(testBoard.getBoard(), playerOne.getData().marker)), null);
+		});
+		it("should switch player move", function() {
+			testBoard.changeTurn();
+			assert.deepEqual(testBoard.isPlayer1Turn(), false);
+		});
 	});
 
 });
