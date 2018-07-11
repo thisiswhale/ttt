94ce4704ac6ec6f26b0e591af59104a71f5fef2e
William Leung <leungwk25@gmail.com>
2018-07-11 14:41:45 -0700

    Refactor code

--- src/index.js
+++ src/index.js
@@ -2,129 +2,155 @@
 const Human = require('./Human.js');
 const Computer = require('./Computer.js');
 const Tictactoe = require('./Tictactoe.js');
+//
+// let test = new Tictactoe();
+// let p = new Human('human', 'x');
+// let ai = new Computer('name', 'o')
+//
+// p.setSquare(test.getBoard(), 0);
+//
+// p.setSquare(test.getBoard(), 1);
+//
+// p.setSquare(test.getBoard(), 2);
 
-let test = new Tictactoe();
-let p = new Human('human', 'x');
-let ai = new Computer('name','o')
-
-p.setSquare(test.getBoard(), 0);
-
-p.setSquare(test.getBoard(), 1);
-
-p.setSquare(test.getBoard(), 2);
-
-console.log(test.hasTie())
-console.log(test.isGameOver(p.isWinner(test.getBoard())))
+// console.log(test.hasTie())
+// console.log(test.isGameOver(p.isWinner(test.getBoard())))
 // console.log(test.getBoard())
 // console.log(p.isWinner(test.getBoard(), p.getData().marker))
 // console.log(test.hasTie())
 
-
 // test.isWinner() || test.hasTie
-class initGame {
-	constructor(setting) {
-		this.board = new Tictactoe();
-		this.player1 = new Human('Human1','x');
-		if( setting =='computer'){
-			this.player2 = new Computer('Computer', 'o');
-		}
-		else{
-			this.player2 = new Human('Human2', 'o');
-		}
-	}
-}
+
 document.addEventListener("DOMContentLoaded", function(event) {
+
+	const squares = document.querySelectorAll('.cell');
+
+	const controlSettings = document.querySelector('#control');
+	controlSettings.onclick = newGame;
+
+	const tableUI = document.querySelector('table');
+
 	let game = new Tictactoe();
 	let grid = game.getBoard();
 
 	let player1 = new Human('Human', 'x');
-	let player2 = new Human('Human', 'o');
+	let player2;
 	let currPlayer;
+	let currMode;
+	newGame();
 
-	const squares = document.querySelectorAll('.cell');
-	const controlSettings = document.querySelector('#control');
-	const tableUI = document.querySelector('table');
-
-	controlSettings.addEventListener('click', newGame);
-	tableUI.addEventListener('click', humanTurn)
-
-	function humanTurn(square){
-
-    let selectCell = square.target;
-
-			if(selectCell.className.includes('cell') &&
-					typeof grid[selectCell.id] == 'number'){
-
-						currPlayer = getPlayerTurn();
-
-				viewSquare(selectCell.id, currPlayer);
-
-				if(!isGameFinished(currPlayer)){
-					game.setPlayerTurn();
-				}
-
-
-				// //Player 2 Computer turn
-				// if (!game.isPlayerOneTurn() && player2.name === 'Computer'){
-				// 	viewSquare(game.getComputerTurnId(player2), player2);
-				// 	viewGameOver(player2);
-				// }
-    }
-  }
-
-	function getPlayerTurn(){
-		if(game.isPlayerOneTurn()){
-			return player1;
+	function setMode(selection) {
+		if (selection === 'computer') {
+			player2 = new Computer('Computer', 'o')
+			currMode = againstComputer;
+		} else {
+			player2 = new Human('Human', 'o');
+			currMode = againstHuman;
 		}
-		else if(!game.isPlayerOneTurn() && player2.name === 'Human'){
-			return player2;
+		tableUI.onclick = currMode;
+	}
+
+	function newGame(event) {
+		game = new Tictactoe();
+		grid = game.getBoard();
+
+		if (event === undefined) {
+			setMode();
+		}
+		else{
+			const node = event.target;
+			//set game to play against human or computer
+			setMode(node.id);
+			displayReset(node.className);
 		}
 	}
 
-	function viewSquare(squareId, player){
-		player.setSquare(grid, squareId);
+	function againstHuman(square) {
+
+		let selectCell = square.target;
+
+		if (selectCell.className.includes('cell') && typeof grid[selectCell.id] == 'number') {
+
+			currPlayer = getPlayerTurn();
+			currPlayer.setSquare(grid, selectCell.id);
+			displaySquare(currPlayer, selectCell.id);
+
+			if (!isGameFinished(currPlayer)) {
+				game.nextPlayerTurn();
+			}
+		}
+	}
+
+	function againstComputer(square) {
+		let selectCell = square.target;
+		console.log('just computer');
+		if (selectCell.className.includes('cell') && typeof grid[selectCell.id] == 'number') {
+
+			currPlayer = getPlayerTurn();
+			currPlayer.setSquare(grid, selectCell.id);
+			displaySquare(currPlayer, selectCell.id);
+
+			if (!isGameFinished(currPlayer)) {
+				game.nextPlayerTurn();
+			}
+			//Player 2 Computer turn
+			// if (currPlayer.name === 'Computer') {
+			// 	currPlayer.setSquare(grid);
+			// 	displaySquare(currPlayer, currPlayer.getMove());
+			//
+			// 	if (!isGameFinished(currPlayer)) {
+			// 		game.nextPlayerTurn();
+			// 	}
+			// }
+		}
+	}
+
+	function getPlayerTurn() {
+		return game.isPlayerOneTurn()
+			? player1
+			: player2;
+	}
+
+	function displaySquare(player, squareId) {
 		let square = document.getElementById(squareId);
 		square.innerText = player.marker;
 		square.className += ' full';
 	}
 
-	function isGameFinished(player){
-
+	function isGameFinished(player) {
+		//expects an object of winner or a boolean value
 		let result = game.isGameOver(player.isWinner(grid));
 
-		console.log(result,grid)
-		if (typeof result === 'object'){
-			result.index.forEach( index =>{
-				squares[index].className += ' win';
-			});
+		if (typeof result === 'object') {
+			displayWin(result);
 			return;
-		}
-		else if(typeof result ==='boolean' && result === true){
-			squares.forEach( cell => {
-				cell.className += ' tie';
-			});
+		} else if (typeof result === 'boolean' && result === true) {
+			displayTie();
 			return;
 		}
 		return false;
 	}
 
-	function newGame(event){
-
-		game = new Tictactoe();
-		grid = game.getBoard();
-
-		const node = event.target.className;
-		if(node.includes('mode'))	{
-			//Removes grid classNames with full, win and tie.
-			squares.forEach( cell => {
-				cell.innerHTML ="";
-				cell.className = cell.className.split(" ").filter(className =>
-					className != 'full' && className !='win' && className != 'tie'
-					).join(" ");
-		 });
-		}
+	function displayWin(winner) {
+		winner.index.forEach(index => {
+			squares[index].className += ' win';
+		});
 	}
 
+	function displayTie() {
+		squares.forEach(cell => {
+			cell.className += ' tie';
+		});
+	}
 
+	function displayReset(node) {
+		//Removes full, win and tie class in the grid
+		if (node.includes('mode')) {
+			squares.forEach(cell => {
+				cell.innerHTML = "";
+				cell.className = cell.className.split(" ").filter(className => className != 'full' && className != 'win' && className != 'tie').join(" ");
+			});
+		}
+	}
 
 });
